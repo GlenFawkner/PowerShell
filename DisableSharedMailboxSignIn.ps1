e8aa0b559bab41f894d5f5c928204cbd7f457cd4
Lief <40873035+GlenFawkner@users.noreply.github.com>
2021-08-20 13:42:30 +1200

    Add files via upload

---
+++ DisableSharedMailboxSignIn.ps1
@@ -0,0 +1,14 @@
+﻿## Use this script to block sign-in for shared mailboxes
+## You must connect to Exchange Online and Azure AD using Connect-EXOPSSession and Connect-MsolService before running this script
+
+
+$SharedMailboxes = Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -Eq "SharedMailbox"}
+
+Foreach ($user in $SharedMailboxes) {
+
+Set-MsolUser -UserPrincipalName $user.UserPrincipalName -BlockCredential $true 
+
+}
+
+
+## End of script
